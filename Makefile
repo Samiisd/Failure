2030b55da539b5c6f680a3c74d0935ebb09adb81
Zachary Fakir <zachary.fakir@epita.fr>
2018-11-10 15:57:16 +0100

    [ANIM] update animation

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC = gcc
 CFLAGS += -Wall -Wextra -Werror -std=c99 -pedantic -g $(shell sdl2-config --cflags)
 LDLIBS += $(shell sdl2-config --libs)
 
-OBJS = src/game.o src/map.o
+OBJS = src/game.o src/map.o src/animation.o
 
 game : $(OBJS)
 	$(CC) $(CFLAGS) $(LDLIBS) -o $@ $^
--- src/animation.c
+++ src/animation.c
@@ -1,22 +1,70 @@
 #include "animation.h"
 
-struct clip
+/*  Decoupage de la feuille de sprites  */
+static void set_clips(struct clip *clip)
 {
-    int SHEET_WIDTH;
-    int SHEET_HEIGHT;
-    int nb_of_sprite;
+    if (!clip)
+    {
+        warnx("struct clip not initialized in set!");
+    }
 
-    //Les surfaces
-    SDL_Surface *faces; //Ns permet de charger et de manipuler notre feuille de sprites.
-    SDL_Surface *screen;
+    clip->left = calloc(clip->nb_of_sprite, sizeof(SDL_Rect));
+    clip->right = calloc(clip->nb_of_sprite, sizeof(SDL_Rect));
+    if (!clip->left || !clip->right)
+    {
+        warnx("struct clip left or right not initialized in set!");
+    }
 
-    //Les différentes parties de la feuille de sprites qui vont être blittés
-    SDL_Rect left[nb_of_sprite]; // qui va garder les pos et les dim de chacun des sprites
-    SDL_Rect right[nb_of_sprite];
-};
+
+    for(int i = 0; i < clip->nb_of_sprite; i++)
+    {
+        clip->right[i].x = clip->SHEET_WIDTH * i;
+        clip->right[i].y = 0;
+        clip->right[i].w = clip->SHEET_WIDTH;
+        clip->right[i].h = clip->SHEET_HEIGHT;
+    }
+
+    for(int i = 0; i < clip->nb_of_sprite; i++)
+    {
+        clip->left[i].x = clip->SHEET_WIDTH * i;
+        clip->left[i].y = clip->SHEET_HEIGHT;
+        clip->left[i].w = clip->SHEET_WIDTH;
+        clip->left[i].h = clip->SHEET_HEIGHT;
+    }
+}
+
+static inline void hero_init(struct clip *clip, char *path)
+{
+    clip->SHEET_WIDTH = 200;
+    clip->SHEET_HEIGHT = 200;
+    clip->nb_of_sprite = 3;
+    clip->frame = 0;
+    clip->faces = SDL_LoadBMP(path);
+    set_clips(clip);
+}
+
+static inline void vilain_init(struct clip *clip, char *path)
+{
+    clip->SHEET_WIDTH = 200;
+    clip->SHEET_HEIGHT = 200;
+    clip->nb_of_sprite = 3;
+    clip->frame = 0;
+    clip->faces = SDL_LoadBMP(path);
+    set_clips(clip);
+}
+
+static inline void cloud_init(struct clip *clip, char *path)
+{
+    clip->SHEET_WIDTH = 200;
+    clip->SHEET_HEIGHT = 200;
+    clip->nb_of_sprite = 3;
+    clip->frame = 0;
+    clip->faces = SDL_LoadBMP(path);
+    set_clips(clip);
+}
 
 /*  initialisation de la struct clip */
-struct clip *init_clip(enum perso perso_type)
+struct clip *init_clip(enum perso_type perso_type, char *path)
 {
     struct clip *clip = calloc(1, sizeof(struct clip));
     if (!clip)
@@ -26,52 +74,41 @@ struct clip *init_clip(enum perso perso_type)
 
     if (perso_type == HERO)
     {
-        clip->SHEET_WIDTH = 200;
-        clip->SHEET_HEIGHT = 200;
-        clip->nb_of_sprite = 3;
-        set_clips(clip);
+        hero_init(clip, path);
     }
 
-    if (perso_type == BAD)
+    if (perso_type == VILAIN)
     {
-        clip->SHEET_WIDTH = 200;
-        clip->SHEET_HEIGHT = 200;
-        clip->nb_of_sprite = 3;
-        set_clips(clip);
+        vilain_init(clip, path);
     }
 
     if (perso_type == CLOUD)
     {
-        clip->SHEET_WIDTH = 200;
-        clip->SHEET_HEIGHT = 200;
-        clip->nb_of_sprite = 3;
-        set_clips(clip);
+        cloud_init(clip, path);
     }
     return clip;
 }
 
-/*  Decoupage de la feuille de sprites  */
-void set_clips(struct clip *clip)
+/*  Change le mouvement du block  */
+void update_anim(struct clip *clip, SDL_Surface *perso_move, enum state cur)
 {
-    if (!clip)
+    SDL_Rect offset;
+    if (cur == LEFT)
     {
-        warnx("struct clip not initialized in set!");
+        offset.x = clip->left[clip->frame].x;
+        offset.y = clip->left[clip->frame].y;
+        clip->frame++;
+    }
+    if (cur == RIGHT)
+    {
+        offset.x = clip->left[clip->frame].x;
+        offset.y = clip->left[clip->frame].y;
+        clip->frame++;
+    }
+    if (clip->frame >= clip->nb_of_sprite)
+    {
+        clip->frame = 0;
     }
 
-    for(int i = 0; i < nb_of_sprite; i++)
-    {
-        clip->right[i].x = clip->SHEET_WIDTH * i;
-        clip->right[i].y = 0;
-        clip->right[i].w = clip->SHEET_WIDTH;
-        clip->right[i].h = clip->SHEET_HEIGHT;
-    }
-
-    for(int i = 0; i < nb_of_sprite; i++)
-    {
-        clip->left[i].x = clip->SHEET_WIDTH * i;
-        clip->left[i].y = clip->SHEET_HEIGHT;
-        clip->left[i].w = clip->SHEET_WIDTH;
-        clip->left[i].h = clip->SHEET_HEIGHT;
-    }
+    SDL_BlitSurface(clip->faces, &offset, perso_move, NULL); // &frame &positionperso
 }
-
